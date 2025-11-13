bb116b931e11eeeae446846e80e928bcd2d6eb37
zZetrium <tomzextra@gmail.com>
2025-11-13 16:03:45 +0100

    why

--- src/main/java/zetrium/daedaluseditor/view/View.java
+++ src/main/java/zetrium/daedaluseditor/view/View.java
@@ -145,7 +145,6 @@ public class View {
                 });
                     updateSelected(true);
                 
-                // TODO alot of stuff
                 //projectFiles.getSelectionModel().selectedItemProperty().;
             }
 
